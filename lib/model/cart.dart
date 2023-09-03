d31ff94a5860c0de42412413fec5d01512d414da
sawsen99 <sounadouma.sd@gmail.com>
2023-09-03 16:11:07 +0200

    generate hiveAdapter review

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -2,8 +2,11 @@ import 'package:cashier_app_ui/model/product.dart';
 import 'package:hive/hive.dart';
 part 'cart.g.dart';
 
+// done: derna la class Cart as a Hive type w mdinalha id = 4
 @HiveType(typeId: 4)
+// done: HiveObject, It means instances of this class can be stored in Hive boxes.
 class Cart extends HiveObject {
+  // done: They specify the order in which the fields should be serialized and deserialized in the box.
   @HiveField(0)
   Product? product;
   @HiveField(1)
--- lib/model/cart.g.dart
+++ lib/model/cart.g.dart
@@ -10,6 +10,7 @@ class CartAdapter extends TypeAdapter<Cart> {
   @override
   final int typeId = 4;
 
+// done: hadi fct read te9ra binary data using BinaryReader w t7awlo l object Cart
   @override
   Cart read(BinaryReader reader) {
     final numOfFields = reader.readByte();
@@ -22,8 +23,10 @@ class CartAdapter extends TypeAdapter<Cart> {
     );
   }
 
+// done: hadi fct write t7awel object Cart l binary data, It writes the fields of the Cart object into the BinaryWriter
   @override
   void write(BinaryWriter writer, Cart obj) {
+    //issue: kifach rahi ta5dem hadi
     writer
       ..writeByte(2)
       ..writeByte(0)
@@ -32,9 +35,10 @@ class CartAdapter extends TypeAdapter<Cart> {
       ..write(obj.quantity);
   }
 
+//issue: wch rah ydir hashCode
   @override
   int get hashCode => typeId.hashCode;
-
+//issue: wch rah ydir operator
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
